5531622eadb278146e1be6373fb8a5bba67edb30
Ashish <ashish@gmail.com>
2025-07-29 15:40:27 +0530

    util file setup

--- src/index.js
+++ src/index.js
@@ -11,7 +11,21 @@ dotenv.config({
 
 
 connectDB()
-
+.then(()=>{
+  app.on("Error",(error) => {
+      console.log("app is not lesten ", error);
+      throw error
+      
+  })
+  app.lesten(process.env.PORT ||  8000, ()=> {
+    console.log(`Server is running at port : ${process.env.PORT}`);
+    
+  })
+})
+.catch((err) =>{
+  console.log("MONGO db connection failed !!!", err);
+  
+})
 
 
 
